d272b4035e215fc65b7c7137c87bcc45f3a317a5
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2024-11-06 10:58:30 -0600

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Police Incident: Report from 2018 to 2020 in San Francisco')
-df = pd.read_csv("Policev1.csv")
+st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
+df=pd.read_csv("Policev1.csv")
 df
-
