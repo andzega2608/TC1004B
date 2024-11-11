07ac4f05fefcf724403078d0ffa76cdca03ff8ff
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2024-11-11 07:27:52 -0600

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,5 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
-df=pd.read_csv("Policev1.csv")
-df
+df = pd.read_csv("Policev1.csv")
+
+st.markdown("The data shown below belogs to incident reports in the city of San Francisco from the year 2018 to 2020, with details fron each case such as date, day of week, police strict, neighborhood in which happened, type of accident in category, exact location and resolution")
+
